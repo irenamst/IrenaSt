dabd29ec6b54394aba708cf4c0b70c452153c31c
irenams <irenamst@gmail.com>
2016-05-10 11:54:54 +0300

    Var2(s ukazateli)

--- Project119/Project119/Zad1(poslednite)(Var2).cpp
+++ Project119/Project119/Zad1(poslednite)(Var2).cpp
@@ -1,18 +1,18 @@
 #include<stdio.h> 
 int *setBit(int x, unsigned char p);
-
+//int mgetline(char s[], int lim);
 int main() {
 	unsigned int x;
-	unsigned int p;
-	
+	unsigned char p;
+	int* x1;
 	scanf_s("%x\n", &x);
-	scanf_s("%u\n", &p);
+	p = getchar();
 
 	printf("%x\n", x);
 	printf("%u\n", p);
 
-	setBit(x,p);
-	//printf("%x", x1);
+	x1=setBit(x,p);
+	printf("%x", x1);
 	getchar();
 	return 0;
 }
@@ -20,3 +20,14 @@ int *setBit(int x, unsigned char p) {
 	int* x1 = (int*)( x | (1 << p) );
 return x1;
 } 
+
+//int mgetline(char s[], int lim) {
+//	int i;
+//	int c;
+//	for (i = 0; i < lim - 1 && ((c = getchar()) != EOF) && c != '\n'; ++i)
+//		s[i] = c;
+//	if (c == '\n')
+//		s[i++] = c;
+//	s[i] = '\0';
+//	return i;
+//}
